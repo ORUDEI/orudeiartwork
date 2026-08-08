094e6aaab5d964af270931977c49ece35044d593
ORUDEI <ald.jo.a@gmail.com>
2026-08-08 11:51:16 -0400

    fix rounting switcher

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,5 +3,9 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['es', 'en'],
   defaultLocale: 'es',
+
   localePrefix: 'as-needed',
+
+  localeDetection: false,
+  localeCookie: false,
 });
